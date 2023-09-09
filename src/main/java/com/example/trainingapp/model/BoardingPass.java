860b126289fb5d3ca596a35b0d83f340a3e0359b
Nicat-dev <nijatmehdiyevv@gmail.com>
2023-09-09 21:19:48 +0400

    Boarding pass

---
+++ src/main/java/com/example/trainingapp/model/BoardingPass.java
@@ -0,0 +1,19 @@
+package com.example.trainingapp.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Table(name = "boarding_pass")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BoardingPass {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
